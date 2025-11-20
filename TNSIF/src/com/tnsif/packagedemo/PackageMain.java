a8fbd787f9858a47734f0a993dfcd39311d2db45
MALATHI K <MALATHI K@192.168.1.18>
2025-11-21 04:16:14 +0530

    pack

--- TNSIF/src/com/tnsif/packagedemo/PackageMain.java
+++ TNSIF/src/com/tnsif/packagedemo/PackageMain.java
@@ -1,5 +1,13 @@
 package com.tnsif.packagedemo;
 
+import com.tnsif.packagesandaccessmodifiers.PackageExample;
+
 public class PackageMain {
+	public static void main(String[] args) {
+
+		PackageExample pe = new PackageExample();
+		pe.display();
+
+	}
 
 }
