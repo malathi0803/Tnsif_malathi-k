641b9c63d9a356d4ee46fe42fb2f2554ef1d92ed
MALATHI K <MALATHI K@LAPTOP-DVAD2P5M>
2025-12-18 11:27:16 +0530

    set

---
+++ TNSIF/src/com/tnsif/collection/set/HashSetDemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.collection.set;
+
+public class HashSetDemo {
+
+	public HashSetDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TNSIF/src/com/tnsif/collection/set/LinkedHashSetDemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.collection.set;
+
+public class LinkedHashSetDemo {
+
+	public LinkedHashSetDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
